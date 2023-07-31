b38efeb3aee9adb167cb2aae2e01d61cba673379
Mikelangelo64 <donni2001331@gmail.com>
2023-07-31 11:59:45 +0300

    test

---
+++ components/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <div class="container-main">
+      <div class="footer-nav">hui</div>
+      <div class="copyright-wrap hg80">
+        <div class="copyright">copyright</div>
+        <div class="footer-socials">huigram</div>
+        <div class="created-by">
+          <a target="_blank" href="">go to hui</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
